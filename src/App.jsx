00ef24e330cb5091bfeafe7c85bfbc7ca8a0e028
Arnab Paul <arnabict@outlook.com>
2026-02-17 16:25:38 +0100

    create routes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { MusicPlayer } from "./components/MusicPlayer";
+import { AllSongs } from "./components/AllSongs";
+import { Playlists } from "./components/Playlists";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <main className="app-main">
+          <div className="player-section">
+            <MusicPlayer />
+          </div>
+          <div className="content-section">
+            <Routes>
+              <Route path="/" element={<AllSongs />} />
+              <Route path="/playlists" element={<Playlists />} />
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
